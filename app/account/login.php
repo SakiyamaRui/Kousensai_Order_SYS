dae097dcc344da52e2332c9545020be1d167844b
sakiyamarui <litogaming1110@gmail.com>
2022-09-29 23:17:33 +0900

    自動ログイン・ログインの復元処理の追加

--- app/account/login.php
+++ app/account/login.php
@@ -2,6 +2,39 @@
     require_once('C:\Users\ic211216\Desktop\創造研究\Kousensai_Order_SYS\app\main.php');
 
     /**
+     * autoLogin
+     */
+    function autoLogin() {
+        session::start();
+        // セッション内にIDがある場合はログイン済み
+        if (isset($_SESSION['store_id'])) {
+            return true;
+        }
+
+        // cookie内にトークンがあるかを確認する
+        if (isset($_COOKIE['store_login_session'])) {
+            // データベースで検索して復元
+            $DB = $DB = DB_Connect();
+            $sql ="SELECT * FROM ORDER_SYS_DB.`T_STORE_TERMINAL_SESSION` WHERE `login_session_id` = :login_session_id";
+            $sql = $DB -> prepare($sql);
+
+            $sql -> bindValue(':login_session_id', $_COOKIE['store_login_session'], PDO::PARAM_STR);
+            $sql -> execute();
+
+            $result = $sql -> fetch();
+
+            if ($result == true) {
+                $_SESSION['store_id'] = $result['store_id'];
+                return true;
+            }
+        }
+
+        // 復元できない場合は転送
+        $URL = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        header("Location: https://kousensai.apori.jp/order/manage/login/?return_to=${URL}");
+    }
+
+    /**
      * Login
      * @param string $username
      * @param string $password
@@ -36,6 +69,8 @@
         if ($passCheck == false) {
             return false;
         }
+
+        unset($DB);
 
         return Array(
             'store_id' => $result['store_account_id'],
