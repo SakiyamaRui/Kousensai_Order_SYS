4df3d1fbda7232cd35604059103e6a1f24539f48
sakiyamarui <litogaming1110@gmail.com>
2022-10-12 14:27:41 +0900

    ログインリクエストのAPIエンドポイントを作成

--- public/api/store.php
+++ public/api/store.php
@@ -1,10 +1,40 @@
 <?php
+    require_once(dirname(__DIR__).'/path.php');
     //
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $request = file_get_contents('php://input');
+        $request = json_decode(file_get_contents('php://input'), true);
     }
 
     switch ($_GET['mode']) {
+        // ログインリクエスト
+        case 'login-request':
+            session::start();
+            if($_SERVER["REQUEST_METHOD"] != "POST"){
+                // 404
+                exit;
+            }
+
+            // ログインリクエストを行う
+            $result = storeLogin($request['user'], $request['password']);
+
+            if ($result == false) {
+                echo 'false';
+                exit;
+            }
+
+            if (isset($_SESSION['return_to'])) {
+                $redirect = $_SESSION['return_to'];
+            }else{
+                $redirect = 'https://'.DOMAIN.'/manage/';
+            }
+
+            $_SESSION['store_id'] = $result['store_id'];
+            $_SESSION['account_name'] = $result['account_name'];
+
+            echo json_encode(Array('redirect' => $redirect));
+            exit;
+            break;
+
         // 注文情報の編集
         case 'getProductDetail':
             break;
