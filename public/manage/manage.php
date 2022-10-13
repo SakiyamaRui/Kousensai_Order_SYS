cad47771175f3e32c9a52d0b5bccbb84abfe011f
sakiyamarui <litogaming1110@gmail.com>
2022-10-13 10:07:40 +0900

    manage.phpのmerge競合対策

--- public/manage/manage.php
+++ public/manage/manage.php
@@ -11,7 +11,31 @@
 
         case 'item':
             $type = '登録';
-            require_once(ROOT_PATH.'/template/item/item-edit.html');
+            if ($request[1] == 'add') {
+
+                // 店舗一覧の取得
+                $DB = DB_Connect();
+                
+                $shop_list = getStore($DB);
+                
+                $template = '<option value="{store_id}">{store_name}</option>';
+                $options = Array();
+                $hide_shop = Array('会計局', 'root');
+                foreach ($shop_list as $val) {
+
+                    if (in_array($val['store_id'], $hide_shop)) {
+                        continue;
+                    }
+
+                    $column = str_replace('{store_id}', $val['store_id'], $template);
+                    $column = str_replace('{store_name}', $val['store_name'], $column);
+                    array_push($options, $column);
+                }
+
+                $shop_options = implode(PHP_EOL, $options);
+                
+                require_once(ROOT_PATH.'/template/item/item-edit.html');
+            }
             break;
 
         case '':
