ea43f769c9fe03a3378c431ea741ca32ed0b01e4
sakiyamarui <litogaming1110@gmail.com>
2022-10-13 17:53:16 +0900

    店舗ログイン用のライブラリを読み込む

--- app/main.php
+++ app/main.php
@@ -25,6 +25,7 @@
     require_once(ROOT_PATH.'/vendor/autoload.php');
     require_once(ROOT_PATH.'/app/DB/database.php');
     require_once(ROOT_PATH.'/app/account/shop.php');
+    require_once(ROOT_PATH.'/app/account/login.php');
     require_once(ROOT_PATH.'/app/secure/token.php');
     require_once(ROOT_PATH.'/app/secure/session.php');
     require_once(ROOT_PATH.'/app/PRODUCT/getData.php');
